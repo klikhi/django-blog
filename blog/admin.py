958b1ffc93919fd89694cb846de18e184473a5fe
klikhi <likhiketan35@gmail.com>
2020-07-21 16:54:14 +0530

    updates

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post, Likes
+from .models import Post, Likes,Comment
 admin.site.register(Post)
 admin.site.register(Likes)
+admin.site.register(Comment)
